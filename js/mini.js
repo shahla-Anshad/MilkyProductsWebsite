0276e08d2f1bd7e1b51298fba9564681a933360a
Shahla_Anshad <shahla13.07.2003>
2025-04-19 15:13:55 +0530

    add validation and email sender

--- js/mini.js
+++ js/mini.js
@@ -9,5 +9,57 @@ document.querySelectorAll(".btn-primary").forEach(button => {
 });
 
 
+// email form sender
+document.addEventListener('DOMContentLoaded', function () {
+document.getElementById('contact-form').addEventListener('submit', async function (event) {
+    event.preventDefault();
+     // Get form elements
+     const name = document.getElementById('name').value.trim();
+     const email = document.getElementById('email').value.trim();
+     const phone = document.getElementById('phonenumber').value.trim();
+     const feedBack = document.getElementById('feedBack').value.trim();
+
+        // Basic validations
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const phonePattern = /^[0-9]{10}$/;
+
+    if (name === '') {
+        alert('Please enter your name.');
+        return;
+    }
+
+    if (!emailPattern.test(email)) {
+        alert('Please enter a valid email address.');
+        return;
+    }
+
+    if (!phonePattern.test(phone)) {
+        alert('Please enter a valid 10-digit phone number.');
+        return;
+    }
+
+
+    // prepare form data
+    const formData = new FormData(event.target);
+    // replace your google sheet api
+    const scriptURL ='https://script.google.com/macros/s/AKfycbzB5qXiMWkh9Q8UIKMtwa3lgENwyTus-7WYzncZvJcTA2j18gUsL8r3tTMj9jgUM6iD/exec';
+
+    // Submit data to google app in google sheet 
+    fetch(scriptURL, {
+        method: 'POST',
+        body: formData,
+    })
+    .then((response) => {
+        alert('Form Submitted Succesfully');
+        document.getElementById('contact-form').reset();
+    })
+     .catch((error) =>{
+        alert('Error submitting the form ')
+        console.error("Error", error)
+     }) 
+});
+});
+
+
 
 
